ce97a36cab8693e00231fbef25c53b976f9fb4df
송지훈 <jihoon.song@medisolution.co.kr>
2023-12-29 11:37:38 +0900

    css 수정 및 작업한거 추가

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["mancity-winners.jpeg", "mancity.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
